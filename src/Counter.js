65720195c03182c85871e022cc9e80fddbc4db53
Suthon Sae-wong <suthon@gmail.com>
2018-02-16 01:52:38 +0700

    module

---
+++ src/Counter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {val: props.initial, incBy: 1}
+
+    this.inc = this.inc.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  inc(){
+    this.setState({val: this.state.val + this.state.incBy})
+  }
+  
+  handleChange(event){
+    this.setState({incBy: parseInt(event.target.value)})
+  }
+
+  render(){
+    return (
+      <div>
+        {this.state.val} 
+        <br/>
+        <input type="number" value={this.state.incBy} onChange={this.handleChange}/>
+        <br/>
+        <button onClick={this.inc}>Click</button>
+      </div>
+    )
+  }
+}
+
+export default Counter;
