6656ddac14adc9ccf30ddb837619a6b06719b2fa
Suthon Sae-wong <suthon@gmail.com>
2018-02-15 23:52:11 +0700

    functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+//functional component
+const Counter = () => (
+  <div>2</div>
+)
 class App extends Component {
   render() {
     return (
       <div>
-        1
+        <Counter/>
       </div>
     );
   }
